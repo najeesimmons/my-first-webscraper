b6d0af4700563f21c2f5fffed5b20bf1610801a4
najeesimmons <najee.bryant.simmons@gmail.com>
2022-11-08 11:21:49 -0500

    added node module to gitignore/ axios now fetching url data

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const PORT = 4000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+const url =
+  "https://www.milehighcomics.com/cgi-bin/backissue.cgi?action=enlarge&issue=24427252592%200";
+axios(url).then((response) => {
+  const html = response.data;
+  console.log(html)
+});
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
